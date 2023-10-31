addbe83bbcb6d249e8e2f118acca697bb8a9c7a5
nimrosh <nimrodka@mta.ac.il>
2023-11-01 00:10:13 +0200

    first commit

---
+++ project/base/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Home page')
+
+
+def room(request):
+    return HttpResponse('room')
+    
